daa1f8f5f1b1345a689608e19e0a6ff7eadc9431
Andy Lindsay <andydlindsay@gmail.com>
2025-01-13 14:00:01 -0800

    hide data endpoints behind /api; return the newly created tweet from the POST request

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const DataHelpers = require("./lib/data-helpers.js")(db);
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+app.use("/api/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
